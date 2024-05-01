bb2013f9ad6464e8479090b674ed15ffe3d404a1
JamesComo44 <21957020+JamesComo44@users.noreply.github.com>
2024-05-01 08:44:40 -0500

    Fix tab length for Flairrel base_stats

--- data/pokemon/base_stats/flairrel.asm
+++ data/pokemon/base_stats/flairrel.asm
@@ -1,24 +1,24 @@
-     db DEX_FLAIRREL ; pokedex id
+    db DEX_FLAIRREL ; pokedex id
 
-     db  65,  85,  60, 101,  95
-     ;   hp  atk  def  spd  spc
+    db  65,  85,  60, 101,  95
+    ;   hp  atk  def  spd  spc
 
-     db FIRE, FIRE ; type
-     db 80  ; catch rate
-     db 165 ; base exp
+    db FIRE, FIRE ; type
+    db 80  ; catch rate
+    db 165 ; base exp
 
-     INCBIN "gfx/pokemon/front/flairrel.pic", 0, 1 ; sprite dimensions
-     dw FlairrelPicFront, FlairrelPicBack
+    INCBIN "gfx/pokemon/front/flairrel.pic", 0, 1 ; sprite dimensions
+    dw FlairrelPicFront, FlairrelPicBack
 
-     db TACKLE, TAIL_WHIP, FIRE_SPIN, NO_MOVE ; level 1 learnset
-     db GROWTH_MEDIUM_SLOW ; growth rate
+    db TACKLE, TAIL_WHIP, FIRE_SPIN, NO_MOVE ; level 1 learnset
+    db GROWTH_MEDIUM_SLOW ; growth rate
 
-     ; tm/hm learnset
-     tmhm RAZOR_WIND,   FLASH,        FIRE_BLAST,   TAKE_DOWN,    DOUBLE_EDGE,  \
-          HYPER_BEAM,   RAGE,         MIMIC,        DOUBLE_TEAM,  SEISMIC_TOSS, \
-          BIDE,         SWIFT,        REST,         SUBSTITUTE,   SKULL_BASH,   \
-          BODY_SLAM,    DRAGON_RAGE,  DIG,          REFLECT,      PAY_DAY
-     ; end
+    ; tm/hm learnset
+    tmhm RAZOR_WIND,   FLASH,        FIRE_BLAST,   TAKE_DOWN,    DOUBLE_EDGE,  \
+        HYPER_BEAM,   RAGE,         MIMIC,        DOUBLE_TEAM,  SEISMIC_TOSS, \
+        BIDE,         SWIFT,        REST,         SUBSTITUTE,   SKULL_BASH,   \
+        BODY_SLAM,    DRAGON_RAGE,  DIG,          REFLECT,      PAY_DAY
+    ; end
 
-     db BANK(FlairrelPicFront)
-     assert BANK(FlairrelPicFront) == BANK(FlairrelPicBack)
+    db BANK(FlairrelPicFront)
+    assert BANK(FlairrelPicFront) == BANK(FlairrelPicBack)
